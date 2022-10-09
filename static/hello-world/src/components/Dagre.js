a0ce86836a7b6d8e3adf6e6bcd7f737de362fdd7
Marcell Munnich <marcellmunnich@gmail.com>
2022-10-09 19:40:47 +1100

    Fixed bugs and refactored code

--- static/hello-world/src/components/Dagre.js
+++ static/hello-world/src/components/Dagre.js
@@ -7,20 +7,18 @@ import ReactFlow, {
 	Controls,
 	MiniMap,
 } from 'react-flow-renderer';
-import dagre from 'dagre';
+import getLayoutedElements from './getLayoutedElements.js';
 // import 'reactflow/dist/style.css';
 
 import { pageNodes, pageEdges } from './nodes-edges.js';
 
 import '../index.css';
 
-console.log('hi');
-// const getNodes =  async () => await pageNodes();
-// const getEdges =  async () => await pageEdges();
-
 const LayoutFlow = () => {
+	// setting the nodes and aedges from the fetched data
 	const [initialNodes, setInitialNodes] = useState([]);
 	const [initialEdges, setInitialEdges] = useState([]);
+
 	useEffect(() => {
 		async function fetchData() {
 			setInitialNodes(await pageNodes());
@@ -30,53 +28,25 @@ const LayoutFlow = () => {
 		fetchData();
 	}, []);
 
-	const dagreGraph = new dagre.graphlib.Graph();
-	dagreGraph.setDefaultEdgeLabel(() => ({}));
+	useEffect(() => {
+		const [layoutNodes, layoutEdges] = getLayoutedElements(
+			initialNodes,
+			initialEdges
+		);
+		setLayoutedNodes(layoutNodes);
+		setLayoutedEdges(layoutEdges);
+		setNodes(layoutedNodes);
+		setEdges(layoutEdges);
+	}, [initialNodes, initialEdges]);
 
-	const nodeWidth = 172;
-	const nodeHeight = 36;
+	const [layoutedNodes, setLayoutedNodes] = useState([]);
+	const [layoutedEdges, setLayoutedEdges] = useState([]);
 
-	function getLayoutedElements(nodes, edges, direction = 'TB') {
-		const isHorizontal = direction === 'LR';
-		dagreGraph.setGraph({ rankdir: direction });
-
-		nodes.forEach((node) => {
-			dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
-		});
-
-		edges.forEach((edge) => {
-			dagreGraph.setEdge(edge.source, edge.target);
-		});
-
-		dagre.layout(dagreGraph);
-
-		nodes.forEach((node) => {
-			const nodeWithPosition = dagreGraph.node(node.id);
-			node.targetPosition = isHorizontal ? 'left' : 'top';
-			node.sourcePosition = isHorizontal ? 'right' : 'bottom';
-
-			// We are shifting the dagre node position (anchor=center center) to the top left
-			// so it matches the React Flow node anchor point (top left).
-			node.position = {
-				x: nodeWithPosition.x - nodeWidth / 2,
-				y: nodeWithPosition.y - nodeHeight / 2,
-			};
-
-			return node;
-		});
-
-		return { nodes, edges };
-	}
-
-	const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
-		initialNodes,
-		initialEdges
-	);
 	console.log('initial: ', initialNodes);
 	console.log('layouted: ', layoutedNodes);
 
-	const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
-	const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+	const [nodes, setNodes, onNodesChange] = useNodesState([]);
+	const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
 	const onConnect = useCallback(
 		(params) =>
@@ -90,10 +60,15 @@ const LayoutFlow = () => {
 	);
 	const onLayout = useCallback(
 		(direction) => {
-			
-			const { nodes: layoutedNodes, edges: layoutedEdges } =
-				getLayoutedElements(initialNodes, initialEdges, direction);
+			const [layoutNodes, layoutEdges] = getLayoutedElements(
+				initialNodes,
+				initialEdges,
+				direction
+			);
+			setLayoutedNodes(layoutNodes);
+			setLayoutedEdges(layoutEdges);
 
+			console.log(layoutedNodes);
 			setNodes([...layoutedNodes]);
 			setEdges([...layoutedEdges]);
 		},
@@ -123,3 +98,4 @@ const LayoutFlow = () => {
 };
 
 export default LayoutFlow;
+
